56eaf2f18c2823256c669a34f882848a3c9fc25a
Artem <arzek2015@gmail.com>
2019-03-01 15:53:10 +0200

    install NgXs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from './state/app.state';
+
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([
+      AppState
+    ]),
+    !environment.production ? NgxsReduxDevtoolsPluginModule.forRoot() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
