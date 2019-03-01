f4a649e30552139618cbc6151d1ff80bc6342ac3
Artem <arzek2015@gmail.com>
2019-03-01 18:43:28 +0200

    html template

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-form',
-  templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.scss']
+  // tslint:disable-next-line:component-selector
+  selector: 'caption[app-todo-form]',
+  templateUrl: './todo-form.component.html'
 })
 export class TodoFormComponent implements OnInit {
 
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  // tslint:disable-next-line:component-selector
+  selector: 'tbody[app-todo-list]',
+  templateUrl: './todo-list.component.html'
 })
 export class TodoListComponent implements OnInit {
 
